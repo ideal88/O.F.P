9d7ec854d228c217d0d5d7d8347e724ef7a82096
ideal88 <idealdandara@gmail.com>
2019-03-15 16:16:14 +0200

    Print out stuff

--- src/main/java/um/ofp/ofp/OfpApplication.java
+++ src/main/java/um/ofp/ofp/OfpApplication.java
@@ -8,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OfpApplication {
 
     public static void main(String[] args) {
-
+        //this is grete
         SpringApplication.run(OfpApplication.class, args);
+        System.out.println("hello");
     }
 
 }
